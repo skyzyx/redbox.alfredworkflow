935d4b3dc4089cc5cf9788d81691495ea2eb9bb0
Ryan Parman <ryan@ryanparman.com>
2013-07-04 17:19:31 -0700

    A few more minor tweaks.

--- workflow.php
+++ workflow.php
@@ -17,15 +17,25 @@ $r = trim(substr(trim($r), 1, -1));
 $r = preg_replace('/\s+/i', ' ', $r);
 $r = json_decode($r, true);
 
+function uc($s)
+{
+	$s = ucwords($s);
+	$s = str_ireplace('blu-ray', 'Blu-ray', $s);
+
+	return $s;
+}
+
 if (count($r) > 0)
 {
 	foreach ($r as $i => $result)
 	{
+		$result = uc($result);
+
 		$wf->result(array(
 			'uid' => sha1($query . $i . time()),
 			'arg' => rawurlencode($result),
-			'title' => ucwords($result),
-			'subtitle' => "Search Redbox for " . ucwords($result),
+			'title' => $result,
+			'subtitle' => "Search Redbox for \"${result}\"",
 			'icon' => __DIR__ . '/icon.png',
 			'valid' => 'yes',
 		));
@@ -36,7 +46,7 @@ else
 	$wf->result(array(
 		'uid' => sha1($query . $i . time()),
 		'arg' => rawurlencode($query),
-		'title' => 'No results found for "' . ucwords($query) . '"',
+		'title' => 'No results found for "' . uc($query) . '"',
 		'subtitle' => "Try using fewer keywords.",
 		'icon' => __DIR__ . '/icon.png',
 		'valid' => 'no',
