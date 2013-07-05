d8d09c0601c8df82c6f7221db5328deb3061aa3b
Ryan Parman <ryan@ryanparman.com>
2013-07-04 17:12:13 -0700

    Fixed a bug with multi-word queries.

--- workflow.php
+++ workflow.php
@@ -7,13 +7,14 @@ use Guzzle\Http\Client;
 $wf = new Workflow('com.ryanparman.workflow.redbox');
 $http = new Client();
 
-$query = $argv[1];
+$query = stripslashes($argv[1]);
 
 $request = $http->get("https://content.atomz.com/autocomplete/sp10/04/a1/b7/?query=" . urlencode($query));
 $r = $request->send()->getBody(true);
 
 // Do some cleanup
 $r = trim(substr(trim($r), 1, -1));
+$r = preg_replace('/\s+/i', ' ', $r);
 $r = json_decode($r, true);
 
 if (count($r) > 0)
@@ -21,10 +22,10 @@ if (count($r) > 0)
 	foreach ($r as $i => $result)
 	{
 		$wf->result(array(
-			'uid' => urlencode($query) . $i . '_' . time(),
+			'uid' => sha1($query . $i . time()),
 			'arg' => rawurlencode($result),
-			'title' => $result,
-			'subtitle' => "Search Redbox for \"${result}\"",
+			'title' => ucwords($result),
+			'subtitle' => "Search Redbox for " . ucwords($result),
 			'icon' => __DIR__ . '/icon.png',
 			'valid' => 'yes',
 		));
@@ -33,9 +34,9 @@ if (count($r) > 0)
 else
 {
 	$wf->result(array(
-		'uid' => urlencode($query) . '_' . time(),
+		'uid' => sha1($query . $i . time()),
 		'arg' => rawurlencode($query),
-		'title' => 'No results found',
+		'title' => 'No results found for "' . ucwords($query) . '"',
 		'subtitle' => "Try using fewer keywords.",
 		'icon' => __DIR__ . '/icon.png',
 		'valid' => 'no',
